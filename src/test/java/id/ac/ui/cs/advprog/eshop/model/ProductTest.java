1c58dc497c13c7e8a4dde78e87ccbdde7eb8e3f5
Andrew Wanarahardja <andrew.wanarahardja@gmail.com>
2026-02-08 15:17:59 +0700

    unit test

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,33 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductTest {
+    Product product;
+
+    @BeforeEach
+    void setup() {
+        this.product = new Product();
+        this.product.setProductId("000000000000-0000-0000-0000-00000000");
+        this.product.setProductName("Shampoo");
+        this.product.setProductQuantity(100);
+    }
+
+    @Test
+    void testGetProductId() {
+        assertEquals("000000000000-0000-0000-0000-00000000", this.product.getProductId());
+    }
+
+    @Test
+    void testGetProductName() {
+        assertEquals("Shampoo", this.product.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantity() {
+        assertEquals(100, this.product.getProductQuantity());
+    }
+}
