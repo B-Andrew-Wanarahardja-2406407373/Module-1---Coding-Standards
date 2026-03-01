d19375c43c9adeb2423d548bcd6616797664fbf3
Andrew Wanarahardja <andrew.wanarahardja@gmail.com>
2026-03-01 14:15:07 +0700

    made car model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.*;
+
+@Getter @Setter
+public class Car {
+    private String carId;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
+}
